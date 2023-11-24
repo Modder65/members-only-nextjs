6e13874a98e02914e5f14a02ba28118aa81063b4
Modder65 <119975910+Modder65@users.noreply.github.com>
2023-11-24 17:20:39 -0500

    Added custom welcome message for user

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,3 +1,4 @@
+/*
 export function Header({ openLoginModal, openSignupModal }) {
   return (
     <header>
@@ -15,4 +16,37 @@ export function Header({ openLoginModal, openSignupModal }) {
       </div>
     </header>
   )
+}
+*/
+
+import { useSession } from "next-auth/react";
+
+export function Header({ openLoginModal, openSignupModal }) {
+  const { data: session } = useSession();
+
+  return (
+    <header>
+      <div className="header-container">
+        <div className="header-title-container">
+          <h1>
+            <span className="members">Members</span>
+            <span className="only">Only</span>
+          </h1>
+        </div>
+        <div className="header-button-container">
+          {!session ? (
+            <>
+              <button className="header-button login-button" onClick={openLoginModal}>Log In</button>
+              <button className="header-button signup-button" onClick={openSignupModal}>Sign Up</button>
+            </>
+          ) : (
+            <div className="user-menu">
+              <span className="user-name">{session.user.name}</span>
+              {/* Dropdown menu will be added here later */}
+            </div>
+          )}
+        </div>
+      </div>
+    </header>
+  );
 }
