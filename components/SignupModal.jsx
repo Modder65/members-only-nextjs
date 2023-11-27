5c8bf8851f107bc6b00d20e0fefefcbec5b6ede4
Modder65 <119975910+Modder65@users.noreply.github.com>
2023-11-27 01:38:47 -0500

    Added better email validation with regex

--- components/SignupModal.jsx
+++ components/SignupModal.jsx
@@ -6,9 +6,13 @@ import ClipLoader from "react-spinners/ClipLoader";
 
 export function SignupModal({ closeModal, showModal }) {
   const [isLoading, setIsLoading] = useState(false);
+  const [emailError, setEmailError] = useState("");
   // Closes the modal when you click on the modal div outside the modal content div
   const modalRef = useRef(null);
   const router = useRouter();
+
+  // Email validation regex 
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
   const handleClick = (event) => {
     if (modalRef.current === event.target) {
@@ -19,7 +23,18 @@ export function SignupModal({ closeModal, showModal }) {
   const handleSubmit = async (event) => {
     event.preventDefault();
     setIsLoading(true);
+    setEmailError(""); // Reset email error
+
     const form = event.target;
+    const email = form.email.value;
+
+    // Check if email matches the regex
+    if (!emailRegex.test(email)) {
+      setEmailError("Please enter a valid email address.");
+      setIsLoading(false);
+      return; // Stop form submission
+    }
+
     const formData = new FormData(form);
     const data = Object.fromEntries(formData.entries());
 
@@ -64,6 +79,7 @@ export function SignupModal({ closeModal, showModal }) {
           <div className="form-group">
             <label htmlFor="email">Email</label>
             <input type="email" name="email" id="email" required/>
+            {emailError && <p className="error-message">{emailError}</p>}
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
