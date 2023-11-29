5dc22b08680ca66a3f4d8be3c511076bd305904f
Modder65 <119975910+Modder65@users.noreply.github.com>
2023-11-28 19:10:50 -0500

    Updated api endpoint for fetching posts so it also fetches the comments for each post

--- app/api/posts/route.js
+++ app/api/posts/route.js
@@ -1,23 +1,29 @@
 import { NextResponse } from "next/server";
 import { connectDB } from "../../../lib/connect-db.js";
 import { PostModel } from "../../../models/post.js";
+import { CommentModel } from "../../../models/comment.js";
 
 export const dynamic = "force-dynamic"; // makes sure the route is dynamic and fetch request always has the latest updated data
                                         // needed when deploying to vercel as it makes the routes static
 
 export async function GET() {
-  console.log("Fetching posts");
   await connectDB();
 
   try {
-    // Fetch posts from database
-    const posts = await PostModel.find().populate("user", "name").exec();
-    console.log("Fetched posts", posts);
+    const posts = await PostModel.find()
+      .populate("user", "name")
+      .lean(); // Use .lean() for faster reads
 
-    // Return the posts as a JSON response
+    // Fetch comments for each post
+    for (let post of posts) {
+      const comments = await CommentModel.find({ post: post._id })
+        .populate("user", "name")
+        .lean();
+      post.comments = comments; // Attach comments to each post
+    }
+
     return NextResponse.json(posts, { status: 200 });
   } catch (error) {
-    // Handle errors
     console.error("Error fetching posts:", error);
     return NextResponse.json({ error: "Internal server error" }, { status: 500 });
   }
