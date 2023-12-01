047bd2b05d8fb527faef29fc933e6e7be8f2e9e9
Modder65 <119975910+Modder65@users.noreply.github.com>
2023-11-30 20:04:26 -0500

    Added logic to fetch the replies of every comment

--- app/api/posts/route.js
+++ app/api/posts/route.js
@@ -2,9 +2,10 @@ import { NextResponse } from "next/server";
 import { connectDB } from "../../../lib/connect-db.js";
 import { PostModel } from "../../../models/post.js";
 import { CommentModel } from "../../../models/comment.js";
+import { ReplyModel } from "../../../models/reply.js";
 
 export const dynamic = "force-dynamic"; // makes sure the route is dynamic and fetch request always has the latest updated data
-                                        // needed when deploying to vercel as it makes the routes static
+                                        // needed when deploying to vercel as it makes the routes static by default
 
 export async function GET() {
   await connectDB();
@@ -14,13 +15,20 @@ export async function GET() {
       .populate("user", "name")
       .lean(); // Use .lean() for faster reads
 
-    // Fetch comments for each post
-    for (let post of posts) {
-      const comments = await CommentModel.find({ post: post._id })
-        .populate("user", "name")
-        .lean();
-      post.comments = comments; // Attach comments to each post
-    }
+      for (let post of posts) {
+        const comments = await CommentModel.find({ post: post._id })
+          .populate("user", "name")
+          .lean();
+        
+        for (let comment of comments) {
+          const replies = await ReplyModel.find({ comment: comment._id })
+            .populate("user", "name")
+            .lean();
+          comment.replies = replies; // Attach replies to each comment
+        }
+  
+        post.comments = comments; // Attach comments to each post
+      }
 
     return NextResponse.json(posts, { status: 200 });
   } catch (error) {
