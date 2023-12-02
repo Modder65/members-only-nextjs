d46a6018f4767f9ccbf99b1fd9343c8e24ef0a00
Modder65 <119975910+Modder65@users.noreply.github.com>
2023-12-02 17:36:47 -0500

    Included pusher context in provider file so its accessible to all children

--- components/Providers.jsx
+++ components/Providers.jsx
@@ -1,7 +1,14 @@
 "use client"
 
 import { SessionProvider } from "next-auth/react";
+import { PusherProvider } from "@/lib/pusherContext";
 
 export function Providers({ children }) {
-  return <SessionProvider>{children}</SessionProvider>
+  return (
+    <SessionProvider>
+      <PusherProvider>
+        {children}
+      </PusherProvider>
+    </SessionProvider>
+  );
 }
