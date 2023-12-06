3b6dfa07c4ee05a21ac0ce595e33a697a5d53034
Modder65 <119975910+Modder65@users.noreply.github.com>
2023-12-05 21:27:30 -0500

    -

--- app/api/posts/route.js
+++ app/api/posts/route.js
@@ -4,10 +4,23 @@ import { NextResponse } from "next/server";
 export const dynamic = "force-dynamic"; // makes sure the route is dynamic and fetch request always has the latest updated data
                                         // needed when deploying to vercel as it makes the routes static by default
 
-export async function GET() {
+export async function GET(request) {
   try {
-    // Fetch only posts with the user who created each post
+    const { searchParams } = new URL(request.url);
+    // Convert page and limit to integers. Default to 0 for page and 10 for limit if not provided
+    const page = parseInt(searchParams.get("page") || "0", 10);
+    const limit = parseInt(searchParams.get("limit") || "10", 10);
+
+    // Calculate the number of posts to skip
+    const skip = page * limit;
+ 
+    // Fetch posts with pagination
     const posts = await prisma.post.findMany({
+      take: limit,     // Number of posts to take
+      skip: skip,      // Number of posts to skip
+      orderBy: {
+        createdAt: 'desc' // Assuming you want newest posts first
+      },
       include: {
         user: { select: { name: true } },
         _count: {
--- app/users/page.jsx
+++ app/users/page.jsx
@@ -6,27 +6,39 @@ import { toast } from "react-hot-toast";
 import axios from "axios";
 import ClipLoader from "react-spinners/ClipLoader";
 import PostList from "./components/PostList";
-
+import InfiniteScroll from "react-infinite-scroll-component";
+import { BsTypeH4 } from "react-icons/bs";
 
 export default function Users() {
   const [posts, setPosts] = useState([]);
+  const [page, setPage] = useState(0);
+  const [hasMore, setHasMore] = useState(true);
   const [isLoading, setIsLoading] = useState(true);
   const { data: session, status } = useSession();
 
-  useEffect(() => {
-    const fetchPosts = async () => {
-      try {
-        const response = await axios.get('/api/posts');
+  const fetchPosts = async () => {
+    try {
+      const limit = 10;
+      const response = await axios.get(`/api/posts?page=${page}&limit=${limit}`);
+      
+      // If page is 0, replace the posts; otherwise, append them
+      if (page === 0) {
         setPosts(response.data);
-      } catch (error) {
-        toast.error("Failed to fetch posts");
-      } finally {
-        setIsLoading(false);
+      } else {
+        setPosts(prevPosts => [...prevPosts, ...response.data]);
       }
-    };
 
+      setHasMore(response.data.length === limit);
+    } catch (error) {
+      toast.error("Failed to fetch posts");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
     fetchPosts();
-  }, []);
+  }, [page]);
 
   if (isLoading) {
     return (
@@ -49,11 +61,23 @@ export default function Users() {
     <div className="mx-auto max-w-6xl px-5 py-5">
       <h2 className="mb-5 text-3xl font-bold">Messages</h2>
       {status === 'authenticated' && session.user ? (
-        <p className="mb-5 text-xl">Welcome <strong>{session.user.name}</strong>!</p>
-      ) : (
-        <p>Welcome to the members only page!</p>
-      )}
-      <PostList posts={posts} />
+          <p className="mb-5 text-xl">Welcome <strong>{session.user.name}</strong>!</p>
+        ) : (
+          <p>Welcome to the members only page!</p>
+        )}
+      <InfiniteScroll
+        dataLength={posts.length}
+        next={() => setPage(prevPage => prevPage + 1)}
+        hasMore={hasMore}
+        loader={<h4>Loading...</h4>}
+        endMessage={
+          <p style={{ textAlign: 'center' }}>
+            <b>There are no more posts</b>
+          </p>
+        }
+      >
+        <PostList posts={posts} />
+      </InfiniteScroll>
     </div>
   );
 }
