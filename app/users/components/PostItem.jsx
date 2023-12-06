67e9a0681573d8104f0f26021347b848dacbb12e
Modder65 <119975910+Modder65@users.noreply.github.com>
2023-12-06 17:54:25 -0500

    Added useEffect import

--- app/users/components/PostItem.jsx
+++ app/users/components/PostItem.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { DateTime } from "luxon";
 import { useForm } from "react-hook-form";
 import { toast } from "react-hot-toast";
