af62f738a0965c13b3a2949c4e8f1157fd7617e0
Modder65 <119975910+Modder65@users.noreply.github.com>
2023-12-06 16:35:46 -0500

    Edited new post message to include an onClick that scrolls user back to top of page

--- Custom-Toast-Messages/notifyNewPost.js
+++ Custom-Toast-Messages/notifyNewPost.js
@@ -1,7 +1,15 @@
 import { toast } from "react-hot-toast";
 
+const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+};
+
 export const notifyNewPost = () =>
     toast('New post available!', {
       icon: '🆕',
-      duration: 2000
+      duration: 2000,
+      onClick: scrollToTop
     });
