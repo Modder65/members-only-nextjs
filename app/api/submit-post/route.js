f930628aaa90827c7464df40a7412e5b9d055707
Modder65 <119975910+Modder65@users.noreply.github.com>
2023-12-07 21:31:57 -0500

    Now uses sanitize-html package to remove potentially harmful characters from inputs and trim whitespace

--- app/api/submit-post/route.js
+++ app/api/submit-post/route.js
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import { pusherServer } from "@/app/libs/pusher";
+import sanitizeHtml from "sanitize-html";
 import prisma from "@/app/libs/prismadb";
 import getSession from "@/app/actions/getSession";
 
@@ -8,21 +9,30 @@ export async function POST(request) {
   const userId = session.user.id;
   const { title, message } = await request.json();
 
-  try {
-    /*
-    const newPost = await prisma.post.create({
-      data: {
-        title, 
-        message, 
-        user: { connect: { id: userId } }
-      }
-    });
-    */
+  // Remove HTML/dangerous characters from data
+  // Trims all whitespace including multiple spaces between words/characters
+  // UI automatically does this so this ensures UI and database match eachother
+  const sanitizedTitle = sanitizeHtml(title, {
+    allowedTags: [],
+    allowedAttributes: {},
+    textFilter: function(text) {
+      return text.replace(/\s+/g, ' ').trim();
+    }
+  });
 
+  const sanitizedMessage = sanitizeHtml(message, {
+    allowedTags: [],
+    allowedAttributes: {},
+    textFilter: function(text) {
+      return text.replace(/\s+/g, ' ').trim();
+    }
+  });
+
+  try {
     const newPost = await prisma.post.create({
       data: {
-        title,
-        message,
+        title: sanitizedTitle,
+        message: sanitizedMessage,
         user: { connect: { id: userId }}
       },
       // Needed to match data structure of fetching posts; for pusher
@@ -38,7 +48,6 @@ export async function POST(request) {
     
     return NextResponse.json({ message: "Post created successfully" }, { status: 200 });
   } catch (error) {
-    console.error("Error saving post", error);
     return NextResponse.json({ error: "Internal server error" }, { status: 500 });
   }
 }
