1c503ac225f9e89bf133eeac811a4396502c4506
Modder65 <119975910+Modder65@users.noreply.github.com>
2023-12-09 16:58:28 -0500

    Made postHandler a useCallback to prevent unecessary re-renders of the function

--- app/users/page.jsx
+++ app/users/page.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { useSession } from "next-auth/react";
 import { toast } from "react-hot-toast";
 import { pusherClient } from "../libs/pusher";
@@ -9,8 +9,7 @@ import { notifyNewPost } from "@/Custom-Toast-Messages/Notify";
 import axios from "axios";
 import ClipLoader from "react-spinners/ClipLoader";
 import PostList from "./components/PostList";
-import InfiniteScroll from "react-infinite-scroll-component";
-
+import { useInView } from "react-intersection-observer";
 
 
 export default function Users() {
@@ -20,48 +19,60 @@ export default function Users() {
   const [isLoading, setIsLoading] = useState(true);
   const { data: session, status } = useSession();
 
-  const fetchPosts = async () => {
-    try {
-      const limit = 10;
-      const response = await axios.get(`/api/posts?page=${page}&limit=${limit}`);
-      
-      // If page is 0, replace the posts; otherwise, append them
-      if (page === 0) {
-        setPosts(response.data);
-      } else {
-        setPosts(prevPosts => [...prevPosts, ...response.data]);
-      }
-
-      setHasMore(response.data.length === limit);
-    } catch (error) {
-      toast.error("Failed to fetch posts");
-    } finally {
-      setIsLoading(false);
-    }
-  };
+  const [ref, inView] = useInView({
+    threshold: 0,
+  });
 
   useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const limit = 10;
+        const response = await axios.get(`/api/posts?page=${page}&limit=${limit}`);
+        
+        // If page is 0, replace the posts; otherwise, append them
+        if (page === 0) {
+          setPosts(response.data);
+        } else {
+          setPosts(prevPosts => [...prevPosts, ...response.data]);
+        }
+  
+        setHasMore(response.data.length === limit);
+      } catch (error) {
+        toast.error("Failed to fetch posts");
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
     fetchPosts();
   }, [page]);
 
   useEffect(() => {
-    pusherClient.subscribe("posts-channel");
+    if (inView && hasMore) {
+      setPage(prevPage => prevPage + 1);
+    }
+  }, [inView, hasMore]);
 
-    const postHandler = (post) => {
-      // Check if the post already exists
-      if (!find(posts, { id: post.id })) {
-        setPosts(current => [post, ...current]); // Prepend new post to the list
-        notifyNewPost();
+  const postHandler = useCallback((post) => {
+    setPosts(current => {
+      // If a post with this ID already exists, don't add it
+      if (!find(current, { id: post.id })) {
+        return [post, ...current];
       }
-    };
+      return current;
+    });
+    notifyNewPost();
+  }, []);
 
+  useEffect(() => {
+    pusherClient.subscribe("posts-channel");
     pusherClient.bind("post:created", postHandler)
 
     return () => {
       pusherClient.unsubscribe("posts-channel");
       pusherClient.unbind("post:created", postHandler);
     }
-  }, [posts]);
+  }, [postHandler]);
 
   if (isLoading) {
     return (
@@ -84,23 +95,16 @@ export default function Users() {
     <div className="mx-auto max-w-6xl px-5 py-5">
       <h2 className="mb-5 text-3xl font-bold">Messages</h2>
       {status === 'authenticated' && session.user ? (
-          <p className="mb-5 text-xl">Welcome <strong>{session.user.name}</strong>!</p>
-        ) : (
-          <p>Welcome to the members only page!</p>
-        )}
-      <InfiniteScroll
-        dataLength={posts.length}
-        next={() => setPage(prevPage => prevPage + 1)}
-        hasMore={hasMore}
-        loader={<h4>Loading...</h4>}
-        endMessage={
-          <p style={{ textAlign: 'center' }}>
-            <b>There are no more posts</b>
-          </p>
-        }
-      >
-        <PostList posts={posts} />
-      </InfiniteScroll>
+        <p className="mb-5 text-xl">Welcome <strong>{session.user.name}</strong>!</p>
+      ) : (
+        <p>Welcome to the members only page!</p>
+      )}
+      <PostList posts={posts} />
+      {hasMore && (
+        <div ref={ref}>
+          <h4>Loading...</h4>
+        </div>
+      )}
     </div>
   );
 }
