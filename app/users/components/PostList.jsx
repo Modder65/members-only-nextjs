b3d43fff34b747f44fcf06ba00e70043a5efd7da
Modder65 <119975910+Modder65@users.noreply.github.com>
2023-12-10 18:00:54 -0500

    Now passes initialLikesCount to PostItem component as prop

--- app/users/components/PostList.jsx
+++ app/users/components/PostList.jsx
@@ -4,7 +4,13 @@ const PostList = ({ posts }) => {
   return (
     <>
       {posts.map(post => (
-        <PostItem key={post.id} post={post} postId={post.id} initialCommentsCount={post._count.comments} />
+        <PostItem 
+        key={post.id} 
+        post={post} 
+        postId={post.id} 
+        initialCommentsCount={post._count.comments} 
+        initialLikesCount={post._count.likes}
+        />
       ))}
     </>
   );
