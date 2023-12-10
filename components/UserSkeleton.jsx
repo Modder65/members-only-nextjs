5be440b89deb29fe0f33e2ad2df28b0d80716109
Modder65 <119975910+Modder65@users.noreply.github.com>
2023-12-09 19:21:50 -0500

    -

--- components/UserSkeleton.jsx
+++ components/UserSkeleton.jsx
@@ -1,6 +1,6 @@
 export function UserSkeleton() {
   return (
-    <div className="animate-pulse flex items-center">
+    <div className="animate-pulse flex items-center gap-1">
       <div className="ml-4 h-6 bg-gray-300 rounded w-24"></div>
       <div className="rounded-full bg-gray-300 h-9 w-9"></div>
     </div>
