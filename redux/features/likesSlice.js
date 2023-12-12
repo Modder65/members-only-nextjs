6434d4d1c277d297df4e0a00fef8375b3775fce2
Modder65 <119975910+Modder65@users.noreply.github.com>
2023-12-12 15:40:01 -0500

    Added redux for state management

---
+++ redux/features/likesSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  posts: {},
+}
+
+export const likesSlice = createSlice({
+  name: "likes",
+  initialState,
+  reducers: {
+    togglePostLike: (state, action) => {
+      const { postId, isLiked, likeCount } = action.payload;
+      state.posts[postId] = { isLiked, likeCount };
+    },
+  },
+});
+
+export const { togglePostLike } = likes.actions;
+export default likesSlice.reducer;
