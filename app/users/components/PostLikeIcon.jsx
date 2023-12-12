9f5ae128a6988e3ad94f4361244fc88619dad30d
Modder65 <119975910+Modder65@users.noreply.github.com>
2023-12-12 15:40:41 -0500

    Now using redux to handle liking of posts

--- app/users/components/PostLikeIcon.jsx
+++ app/users/components/PostLikeIcon.jsx
@@ -9,14 +9,76 @@ import { useSession } from "next-auth/react";
 import clsx from "clsx";
 import gsap from "gsap";
 import axios from "axios";
+import { togglePostLike } from "@/redux/features/likesSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const PostLikeIcon = ({ postId, initialLikesCount, currentUserLiked }) => {
+  const dispatch = useDispatch();
+
+  const { isLiked, likeCount } = useSelector((state) => state.likes.posts[postId]) || {
+    isLiked: currentUserLiked,
+    likeCount: initialLikesCount,
+  };
+
+  const [isLoading, setIsLoading] = useState(false);
+  const heartIconRef = useRef(null);
+  const { data: session } = useSession();
+
+  const handleToggleLike = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await axios.post('/api/like-post', { postId });
+      const { likeCount, userLikedPost } = response.data;
+
+      dispatch(togglePostLike({ postId, isLiked: userLikedPost, likeCount }));
+
+      animateHeartIcon();
+      notifyLike();
+    } catch (error) {
+      toast.error('Error updating like.');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const animateHeartIcon = () => {
+    gsap.fromTo(
+      heartIconRef.current,
+      { scale: 1 },
+      { scale: 1.5, duration: 0.2, ease: 'power1.out', yoyo: true, repeat: 1 }
+    );
+  };
+
+  useEffect(() => {
+    const handleLikeUpdate = (data) => {
+      if (data.postId === postId) {
+        setLikeCount(data.likeCount);
+
+        // Update isLiked only if the action is performed by the current user
+        if (data.actionUserId === session.user.id) {
+          setIsLiked(data.userLikedPost);
+        }
+      }
+    };
+
+    pusherClient.subscribe("likes-channel");
+    pusherClient.bind("post:liked", handleLikeUpdate);
+
+    return () => {
+      pusherClient.unsubscribe("likes-channel");
+      pusherClient.unbind("post:liked", handleLikeUpdate); 
+    };
+  }, [postId, session?.user?.id]);
+
+  /*
   const [isLiked, setIsLiked] = useState(currentUserLiked);
   const [likeCount, setLikeCount] = useState(initialLikesCount);
   const [isLoading, setIsLoading] = useState(false);
   const heartIconRef = useRef(null);
   const { data: session } = useSession();
 
+  
   const toggleLike = async () => {
     setIsLoading(true);
 
@@ -63,13 +125,14 @@ const PostLikeIcon = ({ postId, initialLikesCount, currentUserLiked }) => {
       pusherClient.unbind("post:liked", handleLikeUpdate); 
     };
   }, [postId, session?.user?.id]);
+  */
 
 
   return ( 
     <div className="flex items-center mt-2">
     <button
       className="flex items-center"
-      onClick={toggleLike}
+      onClick={handleToggleLike}
       disabled={isLoading}
     >
       <span ref={heartIconRef} className="text-rose-600">
