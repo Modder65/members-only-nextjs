4f0458388d8a7500b13df1870f130a3171305340
Modder65 <119975910+Modder65@users.noreply.github.com>
2023-12-13 15:11:23 -0500

    Made pushers like updates a provider thats wrapped around my main layout file

---
+++ app/context/PusherContext.jsx
@@ -0,0 +1,55 @@
+import { useEffect } from "react";
+import { pusherClient } from "../libs/pusher";
+import { useDispatch } from "react-redux";
+import { togglePostLike, toggleCommentLike, toggleReplyLike } from "@/redux/features/likesSlice";
+
+
+export const RealTimeProvider = ({ children }) => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // Handler for post likes
+    const handlePostLikeUpdate = (data) => {
+      dispatch(togglePostLike({
+        postId: data.postId,
+        userId: data.actionUserId,
+        isLiked: data.userLikedPost,
+        likeCount: data.likeCount
+      }));
+    };
+
+    // Handler for comment likes
+    const handleCommentLikeUpdate = (data) => {
+      dispatch(toggleCommentLike({
+        commentId: data.commentId,
+        userId: data.actionUserId,
+        isLiked: data.userLikedComment,
+        likeCount: data.likeCount
+      }));
+    };
+
+    // Handler for reply likes
+    const handleReplyLikeUpdate = (data) => {
+      dispatch(toggleReplyLike({
+        replyId: data.replyId,
+        userId: data.actionUserId,
+        isLiked: data.userLikedReply,
+        likeCount: data.likeCount
+      }));
+    };
+
+    pusherClient.subscribe("likes-channel");
+    pusherClient.bind("post:liked", handlePostLikeUpdate);
+    pusherClient.bind("comment:liked", handleCommentLikeUpdate);
+    pusherClient.bind("reply:liked", handleReplyLikeUpdate);
+
+    return () => {
+      pusherClient.unsubscribe("likes-channel");
+      pusherClient.unbind("post:liked", handlePostLikeUpdate);
+      pusherClient.unbind("comment:liked", handleCommentLikeUpdate);
+      pusherClient.unbind("reply:liked", handleReplyLikeUpdate);
+    };
+  }, [dispatch]);
+
+  return <>{children}</>;
+};
