bc382d34807b1ab0a5d20b79b7e5c3521485c762
Modder65 <119975910+Modder65@users.noreply.github.com>
2023-12-12 20:39:22 -0500

    Added liking feature for replies

---
+++ app/api/like-reply/route.js
@@ -0,0 +1,61 @@
+import prisma from "@/app/libs/prismadb";
+import { pusherServer } from "@/app/libs/pusher";
+import getSession from "@/app/actions/getSession";
+import { NextResponse } from "next/server";
+
+export async function POST(request) {
+  const session = await getSession(request);
+  const userId = session.user.id;
+  const { replyId } = await request.json();
+
+  try {
+    let userLikedReply = false;
+
+    // Check if like already exists
+    const existingLike = await prisma.like.findUnique({
+      where: {
+        userId_replyId: {
+          userId,
+          replyId,
+        },
+      },
+    });
+
+    if (existingLike == null) {
+      await prisma.like.create({
+        data: {
+          user: { connect: { id: userId } },
+          reply: { connect: { id: replyId } },
+        },
+      });
+      userLikedReply = true; // User liked the reply
+    } else {
+      await prisma.like.delete({
+        where: { id: existingLike.id },
+      });
+      userLikedReply = false; // User unliked the reply
+    }
+
+    // Get updated like count
+    const updatedLikeCount = await prisma.like.count({
+      where: { replyId },
+    });
+
+    // Pusher broadcast
+    await pusherServer.trigger("likes-channel", "reply:liked", {
+      replyId,
+      likeCount: updatedLikeCount,
+      actionUserId: userId, // User who performed the action
+      userLikedReply // Indicates if the action was like or unlike
+    });
+
+    return NextResponse.json({ 
+      message: "Like updated successfully", 
+      likeCount: updatedLikeCount,
+      userLikedReply: userLikedReply
+  }, { status: 200 });
+  } catch (error) {
+    console.error("Error updating like:", error);
+    return NextResponse.json({ error: error.message || "Internal server error" }, { status: 500 });
+  }
+}
---
+++ app/users/components/ReplyLikeIcon.jsx
@@ -0,0 +1,94 @@
+"use client"
+
+import { useState, useRef, useEffect } from "react";
+import { FiHeart } from "react-icons/fi";
+import { toast } from "react-hot-toast";
+import { notifyLike } from "@/Custom-Toast-Messages/Notify";
+import { pusherClient } from "@/app/libs/pusher";
+import { useSession } from "next-auth/react";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleReplyLike } from "@/redux/features/likesSlice"; 
+import clsx from "clsx";
+import gsap from "gsap";
+import axios from "axios";
+
+const ReplyLikeIcon = ({ replyId, initialLikesCount, currentUserLiked }) => {
+  const dispatch = useDispatch();
+  const { data: session } = useSession();
+  const userId = session?.user?.id;
+
+  const replyLikes = useSelector((state) => state.likes.replies[replyId]);
+  const isLiked = replyLikes?.userLikes[userId] ?? currentUserLiked;
+  const likeCount = replyLikes?.likeCount ?? initialLikesCount;
+
+  const heartIconRef = useRef(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleToggleLike = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await axios.post('/api/like-reply', { replyId });
+      const { likeCount, userLikedComment } = response.data;
+
+      // Dispatch action to update global state
+      dispatch(toggleReplyLike({ replyId, userId, isLiked: userLikedComment, likeCount }));
+
+      animateHeartIcon();
+      notifyLike();
+    } catch (error) {
+      toast.error('Error updating like.');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  
+  const animateHeartIcon = () => {
+    gsap.fromTo(
+      heartIconRef.current,
+      { scale: 1 },
+      { scale: 1.5, duration: 0.2, ease: 'power1.out', yoyo: true, repeat: 1 }
+    );
+  };
+
+  useEffect(() => {
+    const handleLikeUpdate = (data) => {
+      if (data.replyId === replyId) {
+        // Dispatch action for real-time updates
+        dispatch(toggleReplyLike({
+          replyId: data.replyId,
+          userId: data.actionUserId,
+          isLiked: data.userLikedReply,
+          likeCount: data.likeCount
+        }));
+      }
+    };
+
+    pusherClient.subscribe("likes-channel");
+    pusherClient.bind("reply:liked", handleLikeUpdate);
+
+    return () => {
+      pusherClient.unsubscribe("likes-channel");
+      pusherClient.unbind("reply:liked", handleLikeUpdate);
+    };
+  }, [dispatch]);
+
+  return ( 
+    <div className="flex justify-end items-center">
+      <button
+        className="flex items-center"
+        onClick={handleToggleLike}
+        disabled={isLoading}
+      >
+        <span ref={heartIconRef} className="text-rose-600">
+          <FiHeart className={clsx('mr-1', {'fill-current': isLiked})} />
+        </span>
+        <span style={{ opacity: likeCount > 0 ? 1 : 0 }}>
+          {likeCount}
+        </span>
+      </button>
+    </div>
+   );
+}
+ 
+export default ReplyLikeIcon;
