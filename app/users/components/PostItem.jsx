e382f9b83faeac4c40bf498c3735d83a440de037
Modder65 <119975910+Modder65@users.noreply.github.com>
2023-12-14 17:54:56 -0500

    -

--- app/users/components/PostItem.jsx
+++ app/users/components/PostItem.jsx
@@ -2,55 +2,27 @@
 
 import { useState, useEffect, useRef, useCallback } from "react";
 import { DateTime } from "luxon";
-import { useForm } from "react-hook-form";
 import { toast } from "react-hot-toast";
 import { FiMessageSquare } from "react-icons/fi";
 import { pusherClient } from "@/app/libs/pusher";
 import { notifyNewComment } from "@/Custom-Toast-Messages/Notify";
-import CommentsSection from "./CommentsSection";
-import Input from "@/app/components/inputs/Input";
-import Button from "@/app/components/Button";
 import axios from "axios";
 import PostLikeIcon from "./PostLikeIcon";
+import PostModal from "./PostModal";
 
 
 const PostItem = ({ post, postId, initialCommentsCount, initialLikesCount, currentUserLiked }) => {
   const [comments, setComments] = useState([]);
   const [commentsLoaded, setCommentsLoaded] = useState(false);
   const [commentCount, setCommentCount] = useState(initialCommentsCount);
-  const [showComments, setShowComments] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [initialFetchDone, setInitialFetchDone] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
-  const { register, handleSubmit, formState: { errors } } = useForm({
-    defaultValues: { message: '' }
-  });
-
-  const onSubmit = async (data) => {
-    setIsLoading(true);
-
-    try {
-      await axios.post('/api/submit-comment', { ...data, postId });
-      toast.success("Comment submitted successfully!")
-    } catch (error) {
-      let errorMessage = "An unexpected error occurred. Please try again.";
-
-      if (error.response) {
-        errorMessage = `Server Error: ${error.response.status}. ${error.response.data.message || ''}`;
-      } else if (error.request) {
-        errorMessage = "Network Error: Unable to reach the server. Please check your connection.";
-      } else {
-        errorMessage = `Error: ${error.message}`;
-      }
-
-      toast.error(errorMessage);
-    } finally {
-      setIsLoading(false);
-    }
-  }
-
+  
   
 
-  const fetchComments = async (postId) => {
+  const fetchComments = async () => {
     // Check if comments have already been loaded or if there are no comments to load
     if (!commentsLoaded && initialCommentsCount > 0) {
       setIsLoading(true);
@@ -66,13 +38,12 @@ const PostItem = ({ post, postId, initialCommentsCount, initialLikesCount, curre
     }
   };
 
-  const toggleCommentsDisplay = async () => {
-    // Fetch comments only if they are not currently shown
-    if (!showComments) {
-      await fetchComments(postId);
-    }
-    setShowComments(!showComments);
+  const openModal = async () => {
+    await fetchComments();
+    setShowModal(true);
   }
+
+  const closeModal = () => setShowModal(false);
 
   const commentHandler = useCallback((comment) => {
     if (comment.postId === postId) {
@@ -98,7 +69,6 @@ const PostItem = ({ post, postId, initialCommentsCount, initialLikesCount, curre
     }
   }, [commentHandler]);
 
- 
   return (
     <div className="max-auto max-w-6xl px-5 mb-5">
       <div className="bg-white border border-gray-300 rounded-lg p-4 shadow-sm">
@@ -113,39 +83,23 @@ const PostItem = ({ post, postId, initialCommentsCount, initialLikesCount, curre
           }
         </p>
         <div className="flex justify-between items-center mt-3">
-          <button onClick={toggleCommentsDisplay}
+          <button onClick={openModal}
            className="bg-green-600 rounded-md px-2 py-1 text-white hover:opacity-80 flex items-center"
            >
             <FiMessageSquare className="mr-2" />
-            {showComments ? `Hide Comments (${commentCount})` : `Show Comments (${commentCount})`}
+            {`Show Comments (${commentCount})`}
           </button>
           <PostLikeIcon postId={postId} initialLikesCount={initialLikesCount} currentUserLiked={currentUserLiked}/>
         </div>
-        {showComments && (
-          <>
-            <form onSubmit={handleSubmit(onSubmit)} className="space-y-6 mt-4">
-              <Input 
-                id="message" 
-                label="Your Comment" 
-                register={register}
-                validation={{
-                  required: "Message is required",
-                  minLength: { value: 4, message: "Message must be at least 4 characters long" },
-                  maxLength: { value: 280, message: "Message has 280 character limit"}
-                }}
-                errors={errors}
-                disabled={isLoading}
-              />
-              <Button
-                disabled={isLoading}
-                fullWidth
-                type="submit"
-              >
-                Submit Comment
-              </Button>
-            </form>
-            <CommentsSection comments={comments} />
-          </>
+        {showModal && (
+          <PostModal 
+          post={post} 
+          postId={post.id} 
+          isOpen={showModal}
+          onClose={closeModal} 
+          comments={comments}
+          setComments={setComments}
+          />
         )}
       </div>
     </div>
