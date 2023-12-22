6fb042434a5003746e788335453857c975f71cc0
Modder65 <119975910+Modder65@users.noreply.github.com>
2023-12-22 16:53:57 -0500

    Added dynamic route for viewing other user pages

---
+++ app/users/[id]/page.jsx
@@ -0,0 +1,107 @@
+"use client"
+
+import { useParams } from 'next/navigation';
+import { Fragment, useEffect, useState } from 'react';
+import { Tab } from '@headlessui/react'
+import { toast } from "react-hot-toast";
+import { useInView } from "react-intersection-observer";
+import axios from 'axios';
+import Avatar from '../components/Avatar';
+import PostList from '../components/PostList';
+import { CustomLoader } from '@/components/CustomLoader';
+
+const UserProfile = () => {
+  const params = useParams();
+  const userId = params.id;
+  const [selectedTabIndex, setSelectedTabIndex] = useState(0);
+  const [hasMore, setHasMore] = useState(true);
+  const [loading, setIsLoading] = useState(false);
+  const [userData, setUserData] = useState(null);
+  
+  const [ref, inView] = useInView({
+    threshold: 0,
+  });
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      setIsLoading(true);
+      try {
+        const response = await axios.get(`/api/user-data?userId=${userId}`);
+        setUserData(response.data);
+        console.log(response.data); // remove
+      } catch (error) {
+        toast.error("Failed to fetch user data");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchUserData();
+  }, [userId]);
+
+  return (
+    <div className="mx-auto max-w-6xl px-5 py-5">
+      <div className='flex gap-2 items-center mb-5'>
+        <Avatar user={userData}/>
+        <p className="text-lg font-semibold">{userData?.name}</p>
+      </div>
+      <div>
+        <Tab.Group selectedIndex={selectedTabIndex} onChange={setSelectedTabIndex}>
+          <Tab.List className="flex gap-5 mb-5 border-b-2">
+            {/* Use the `tab` function to apply styles dynamically */}
+            <Tab as={Fragment}>
+              {({ selected }) => (
+                <button
+                  className={`py-2 px-4 text-sm font-medium leading-5 text-gray-700 rounded-t-lg focus:outline-none focus:ring-2 ring-white ring-opacity-60 ${
+                    selected ? 'border-b-2 border-blue-600 bg-white' : 'hover:bg-gray-100'
+                  }`}
+                >
+                  About
+                </button>
+              )}
+            </Tab>
+            <Tab as={Fragment}>
+              {({ selected }) => (
+                <button
+                  className={`py-2 px-4 text-sm font-medium leading-5 text-gray-700 rounded-t-lg focus:outline-none focus:ring-2 ring-white ring-opacity-60 ${
+                    selected ? 'border-b-2 border-blue-600 bg-white' : 'hover:bg-gray-100'
+                  }`}
+                >
+                  Friends
+                </button>
+              )}
+            </Tab>
+            <Tab as={Fragment}>
+              {({ selected }) => (
+                <button
+                  className={`py-2 px-4 text-sm font-medium leading-5 text-gray-700 rounded-t-lg focus:outline-none focus:ring-2 ring-white ring-opacity-60 ${
+                    selected ? 'border-b-2 border-blue-600 bg-white' : 'hover:bg-gray-100'
+                  }`}
+                >
+                  Posts
+                </button>
+              )}
+            </Tab>
+            {/* Repeat for other tabs */}
+          </Tab.List>
+          <Tab.Panels>
+            <Tab.Panel>Content 1</Tab.Panel>
+            <Tab.Panel>Content 2</Tab.Panel>
+            <Tab.Panel>
+              <PostList posts={userData?.posts}/>
+                {hasMore ? (
+                  <div ref={ref} className="flex justify-center">
+                    <CustomLoader />
+                  </div>
+                ) : (
+                  <p className="text-center font-semibold text-xl mt-5">There are no more posts.</p>
+                )}
+            </Tab.Panel>
+          </Tab.Panels>
+        </Tab.Group>
+      </div>
+    </div>
+  );
+};
+
+export default UserProfile;
