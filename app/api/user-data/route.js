ff996e7ab87eeda65cf9858dcf4ac33ca96ae668
Modder65 <119975910+Modder65@users.noreply.github.com>
2023-12-26 18:41:35 -0500

    Now gets the current friendshipStatus between the logged in user and profile user

--- app/api/user-data/route.js
+++ app/api/user-data/route.js
@@ -1,4 +1,5 @@
 import prisma from "@/app/libs/prismadb";
+import getSession from "@/app/actions/getSession";
 import { NextResponse } from "next/server";
 
 
@@ -7,22 +8,50 @@ export async function GET(request) {
   try {
     const { searchParams } = new URL(request.url);
     const userId = searchParams.get("userId");
+    const session = await getSession();
+    const loggedInUserId = session.user.id;
 
-    // Fetch user data and their posts
     const userData = await prisma.user.findUnique({
       where: { id: userId },
       include: {
-        posts: { // Assuming 'posts' is the field name in your Prisma schema
+        posts: {
           orderBy: { createdAt: 'desc' },
           include: {
             user: { select: { id: true, name: true, image: true } },
             _count: { select: { comments: true, likes: true } },
           }
         },
+        userFriendships: {
+          where: { status: 'ACCEPTED' },
+          include: {
+            friend: { select: { id: true, name: true, image: true } }
+          }
+        },
+        friendUserFriendships: {
+          where: { status: 'ACCEPTED' },
+          include: {
+            user: { select: { id: true, name: true, image: true } }
+          }
+        }
       }
     });
 
-    return NextResponse.json(userData, { status: 200 });
+    // Check the friendship status between logged-in user and profile user
+    let friendshipStatus = null;
+    const friendship = await prisma.friendship.findFirst({
+      where: {
+        OR: [
+          { userId: loggedInUserId, friendId: userId },
+          { userId: userId, friendId: loggedInUserId }
+        ]
+      }
+    });
+
+    if (friendship) {
+      friendshipStatus = friendship.status;
+    }
+
+    return NextResponse.json({userData, friendshipStatus}, { status: 200 });
   } catch (error) {
     console.error("Error fetching user data:", error);
     return NextResponse.json({ error: "Internal server error" }, { status: 500 });
