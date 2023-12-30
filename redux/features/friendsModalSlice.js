c4f2bbe7c38223bc619441b463e2713d60a0f3ca
Modder65 <119975910+Modder65@users.noreply.github.com>
2023-12-30 13:40:27 -0500

    Added new friendsModalSlice to redux to handle friendsModal state

---
+++ redux/features/friendsModalSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  isModalOpen: false,
+};
+
+export const friendsModalSlice = createSlice({
+  name: 'friendsModal',
+  initialState,
+  reducers: {
+    // Reducer for opening the modal and setting the selected post.
+    openModal: (state, action) => {
+      state.isModalOpen = true;
+    },
+
+    // Reducer for closing the modal
+    closeModal: (state) => {
+      state.isModalOpen = false;
+    },
+  },
+});
+
+// Export the action creators for the slice
+export const { openModal, closeModal } = friendsModalSlice.actions;
+
+// Export the reducer function for the slice
+export default friendsModalSlice.reducer;
