a69f7d18631cfc056c6843016f29836487b18fd4
Modder65 <119975910+Modder65@users.noreply.github.com>
2023-12-31 02:58:35 -0500

    Moved friendmodal here instead

--- app/layout.jsx
+++ app/layout.jsx
@@ -24,6 +24,7 @@ export default function RootLayout({ children }) {
             </RealTimeProvider>
           </AuthContext>
         </ReduxProvider>
+        <FriendsModal />
       </body>
     </html>
   )
