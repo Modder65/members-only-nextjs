d51d11fec34b302763b62476de94c7c65872c6ae
Modder65 <119975910+Modder65@users.noreply.github.com>
2024-01-03 23:37:15 -0500

    Removed unecessary session calls

--- app/users/page.jsx
+++ app/users/page.jsx
@@ -1,24 +1,25 @@
 'use client'
 
 import { useState, useEffect, useCallback, useRef } from "react";
-import { useSession } from "next-auth/react";
 import { toast } from "react-hot-toast";
 import { pusherClient } from "../../lib/pusher";
 import { find } from "lodash";
 import { notifyNewPost } from "@/Custom-Toast-Messages/Notify";
 import { useInView } from "react-intersection-observer";
-import { CustomLoader } from "@/components/CustomLoader";
+import { BeatLoader } from "react-spinners";
+import { useCurrentUser } from "@/hooks/use-current-user";
 import axios from "axios";
 import ClipLoader from "react-spinners/ClipLoader";
 import PostList from "./components/PostList";
-import Image from "next/image";
+
 
 export default function Users() {
   const [posts, setPosts] = useState([]);
   const [page, setPage] = useState(0);
   const [hasMore, setHasMore] = useState(true);
   const [isLoading, setIsLoading] = useState(true);
-  const { data: session, status } = useSession();
+
+  const user = useCurrentUser();
 
   const [ref, inView] = useInView({
     threshold: 0,
@@ -95,15 +96,15 @@ export default function Users() {
   return (
     <div className="mx-auto max-w-6xl px-5 py-5">
       <h2 className="mb-5 text-3xl font-bold">Posts</h2>
-      {status === 'authenticated' && session.user ? (
-        <p className="mb-5 text-xl">Welcome <strong>{session.user.name}</strong>!</p>
+      {user ? (
+        <p className="mb-5 text-xl">Welcome <strong>{user.name}</strong>!</p>
       ) : (
         <p>Welcome to the members only page!</p>
       )}
       <PostList posts={posts} />
       {hasMore ? (
         <div ref={ref} className="flex justify-center">
-          <CustomLoader />
+          <BeatLoader />
         </div>
       ) : (
         <p className="text-center font-semibold text-xl mt-5">There are no more posts.</p>
