665d08a9010add1b78ab08695a70e9b048a78ff8
Modder65 <119975910+Modder65@users.noreply.github.com>
2024-01-07 16:36:53 -0500

    Navbar is now position fixed so it never moves on the y-axis

--- app/(protected)/_components/navbar.jsx
+++ app/(protected)/_components/navbar.jsx
@@ -12,7 +12,7 @@ export const Navbar = () => {
 
   return (
     <nav className="bg-secondary flex justify-between
-     items-center p-4 rounded-xl w-[600px] shadow-sm">
+     items-center p-4 rounded-xl w-[600px] shadow-sm fixed top-10">
       <div className="flex gap-x-2">
         <Button
           asChild
