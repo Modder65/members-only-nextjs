6d9a049225ffbd3665fa8ae189272edd2b0be62e
Modder65 <119975910+Modder65@users.noreply.github.com>
2024-01-12 02:41:21 -0500

    Fixed navbar content centering vertically incorrectly

--- app/(protected)/_components/navbar.jsx
+++ app/(protected)/_components/navbar.jsx
@@ -12,7 +12,7 @@ export const Navbar = () => {
 
   return (
     <nav className="bg-secondary flex justify-between
-     items-center p-4 rounded-xl w-[600px] shadow-sm fixed top-10">
+     items-center p-4 rounded-xl w-[600px] shadow-sm">
       <div className="flex gap-x-2">
         <Button
           asChild
--- app/(protected)/layout.jsx
+++ app/(protected)/layout.jsx
@@ -3,8 +3,8 @@ import { Navbar } from "./_components/navbar";
 const ProtectedLayout = ({children}) => {
   return ( 
     <div className="h-full w-full flex flex-col gap-y-10 items-center 
-    justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))]
-    from-green-400 to-green-800">
+     bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))]
+    from-green-400 to-green-800 pt-[4rem]">
       <Navbar />
       {children}
     </div>
