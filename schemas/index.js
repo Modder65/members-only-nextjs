54c4a953149da28e2a77aacb0201e526b719c4ab
Modder65 <119975910+Modder65@users.noreply.github.com>
2024-01-29 17:57:03 -0500

    Removed user-role field from settings schema

--- schemas/index.js
+++ schemas/index.js
@@ -4,7 +4,6 @@ import * as z from "zod";
 export const SettingsSchema = z.object({
   name: z.optional(z.string()),
   isTwoFactorEnabled: z.optional(z.boolean()),
-  role: z.enum([UserRole.OWNER, UserRole.ADMIN, UserRole.USER]),
   email: z.optional(z.string().email()),
   password: z.optional(z.string().min(6)),
   newPassword:z.optional(z.string().min(6)),
