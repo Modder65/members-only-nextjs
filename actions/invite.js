eb8b647edb012b50b0e68c517ddff21b8184f4f6
Modder65 <119975910+Modder65@users.noreply.github.com>
2024-01-30 23:49:21 -0500

    Added server-side check to make sure a user has the OWNER role before allowing them to send someone an invite to register

--- actions/invite.js
+++ actions/invite.js
@@ -3,8 +3,16 @@
 import { generateInviteToken } from "@/lib/tokens";
 import { InvitationSchema } from "@/schemas";
 import { sendInvitationEmail } from "@/lib/mail";
+import { currentRole } from "@/lib/auth";
+import { UserRole } from "@prisma/client";
 
 export const invite = async (values) => {
+  const role = await currentRole();
+
+  if (role !== UserRole.OWNER) {
+    return { error: "You do not have permission to send invites!" };
+  }
+
   const validatedFields = InvitationSchema.safeParse(values);
 
   const { email } = validatedFields.data;
