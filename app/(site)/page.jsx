0238e8cb276e97157b84cb81fe81e7e32cec8abb
Modder65 <119975910+Modder65@users.noreply.github.com>
2024-02-27 15:03:56 -0500

    Fixing mobile styling

--- app/(site)/page.jsx
+++ app/(site)/page.jsx
@@ -35,7 +35,7 @@ export default function Home() {
   }
 
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen p-4">
+    <main className="flex flex-col items-center justify-center min-h-screen p-2 overflow-hidden">
         <div className="
           space-y-6 
           text-center 
@@ -43,21 +43,19 @@ export default function Home() {
           max-w-xs 
           sm:max-w-sm
           md:max-w-md
-          lg:max-w-lg
-          xl:max-w-xl
         ">
           <Card className="w-full">
             <CardHeader className="flex flex-col gap-y-2">
-              <h1 className="text-2xl font-bold sm:text-3xl md:text-4xl">
+              <h1 className="text-3xl md:text-4xl font-bold">
                 <span className="text-skin-header">Members</span>
                 <span className="text-black">Only</span>
               </h1>
-              <p className="text-skin-base text-sm sm:text-md md:text-lg">
+              <p className="text-skin-base text-lg">
                 A Private Social Media Platform
               </p>
             </CardHeader>
-            <CardContent className="p-4 sm:p-6">
-              <div className="pt-0 mb-7">
+            <CardContent>
+              <div className="p-6 pt-0">
                 <LoginButton mode="modal" asChild>
                   <Button size="lg">
                     Sign In
@@ -66,23 +64,19 @@ export default function Home() {
               </div>
               <p className="
                 text-skin-base
-                text-sm
-                sm:text-md  
-                md:text-lg
+                text-lg
                 mb-4
               ">Change Theme</p>
               <div className="
                 flex
-                gap-x-3
-                sm:gap-x-4
-                md:gap-x-5
+                gap-x-5
                 justify-center
               ">
-                <div className="w-4 h-4 sm:w-5 sm:h-5 md:w-6 md:h-6 bg-bluetheme opacity-80 shadow rounded cursor-pointer" onClick={() => changeTheme('theme-blue')}></div>
-                <div className="w-4 h-4 sm:w-5 sm:h-5 md:w-6 md:h-6 bg-greentheme opacity-80 shadow rounded cursor-pointer" onClick={() => changeTheme('theme-green')}></div>
-                <div className="w-4 h-4 sm:w-5 sm:h-5 md:w-6 md:h-6 bg-yellowtheme opacity-80 shadow rounded cursor-pointer" onClick={() => changeTheme('theme-yellow')}></div>
-                <div className="w-4 h-4 sm:w-5 sm:h-5 md:w-6 md:h-6 bg-redtheme opacity-80 shadow rounded cursor-pointer" onClick={() => changeTheme('theme-red')}></div>
-                <div className="w-4 h-4 sm:w-5 sm:h-5 md:w-6 md:h-6 bg-purpletheme opacity-80 shadow rounded cursor-pointer" onClick={() => changeTheme('theme-purple')}></div>
+                <div className="w-6 h-6 bg-bluetheme opacity-80 shadow rounded cursor-pointer" onClick={() => changeTheme('theme-blue')}></div>
+                <div className="w-6 h-6 bg-greentheme opacity-80 shadow rounded cursor-pointer" onClick={() => changeTheme('theme-green')}></div>
+                <div className="w-6 h-6 bg-yellowtheme opacity-80 shadow rounded cursor-pointer" onClick={() => changeTheme('theme-yellow')}></div>
+                <div className="w-6 h-6 bg-redtheme opacity-80 shadow rounded cursor-pointer" onClick={() => changeTheme('theme-red')}></div>
+                <div className="w-6 h-6 bg-purpletheme opacity-80 shadow rounded cursor-pointer" onClick={() => changeTheme('theme-purple')}></div>
               </div>
             </CardContent>
           </Card>
